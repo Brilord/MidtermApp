2b7327bd90abc5aae422d4b7900f38898e780c65
Brilord <brilord1000@gmail.com>
2022-10-11 22:12:48 -0400

    f

--- app/src/main/java/com/example/midtermapp/HighScoreActivity.java
+++ app/src/main/java/com/example/midtermapp/HighScoreActivity.java
@@ -3,9 +3,24 @@ package com.example.midtermapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+import com.example.midtermapp.sqlDB.DBManager;
+import com.example.midtermapp.sqlDB.DatabaseHelper;
 
 public class HighScoreActivity extends AppCompatActivity {
+    private DBManager dbManager;
+    private ListView listView;
+    private SimpleCursorAdapter adopter;
 
+    final String[] from = new String[] {
+            DatabaseHelper.ID, DatabaseHelper.NAME, DatabaseHelper.SCORE
+    };
+
+    final int[] to = new int[] {
+            R.id.id, R.id.title, R.id.
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
