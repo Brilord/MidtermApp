e7efd445fb4ae5bf9d94fed4b3aa2ec24a300ef3
Brilord <95247252+Brilord@users.noreply.github.com>
2022-10-13 03:38:28 -0400

    yy

--- app/src/main/java/com/example/midtermapp/Adopter/MyAdopter.java
+++ app/src/main/java/com/example/midtermapp/Adopter/MyAdopter.java
@@ -1,42 +1,52 @@
 package com.example.midtermapp.Adopter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class MyAdopter extends RecyclerView.Adapter<MyAdopter.MyViewHolder> {
-    String data1[], data2[];
-    int image[];
-    Context context;
+import com.example.midtermapp.R;
 
-    public MyAdopter(Context ct, String s1[], String s2[], int img[]) {
-        context = ct;
-        data1 = s1;
-        data2 = s2;
-        image = img;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyAdopter extends RecyclerView.Adapter<MyAdopter.MyViewHolder> {
+    private ArrayList<User> usersList;
+
+    public MyAdopter(ArrayList<User> usersList) {
+        this.usersList = usersList;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        private TextView nameText;
+
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            nameText = itemView.findViewById(R.id.highscore_list);
+        }
     }
     @NonNull
     @Override
     public MyAdopter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_high_score, parent, false);
+        return new MyViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyAdopter.MyViewHolder holder, int position) {
-
+        String name = usersList.get(position).getUsername();
     }
 
     @Override
     public int getItemCount() {
-        return 0;
-    }
-
-    public class MyViewHolder extends RecyclerView.ViewHolder{
-        public MyViewHolder(@NonNull View itemView) {
-            super(itemView);
-        }
+        return usersList.size();
     }
 }
+
+
--- app/src/main/java/com/example/midtermapp/HighScoreActivity.java
+++ app/src/main/java/com/example/midtermapp/HighScoreActivity.java
@@ -1,6 +1,8 @@
 package com.example.midtermapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.database.Cursor;
@@ -12,62 +14,44 @@ import android.widget.SimpleCursorAdapter;
 
 //import com.example.midtermapp.Adopter.CustomAdapter;
 //import com.example.midtermapp.sqlDB.DBManager;
+import com.example.midtermapp.Adopter.MyAdopter;
+import com.example.midtermapp.Adopter.User;
 import com.example.midtermapp.sqlDB.DBOpenHelper;
 //import com.example.midtermapp.sqlDB.DatabaseHelper;
 
 import java.util.ArrayList;
 
 public class HighScoreActivity extends AppCompatActivity {
-    //private DBManager dbManager;
-    private ListView listView;
-    private SimpleCursorAdapter adopter;
+
+    MyAdopter adopter;
     RecyclerView recyclerView;
     //CustomAdapter customAdopter;
-    ArrayList<String> noteslist = new ArrayList<>();
-    ArrayList<String> playerName = new ArrayList<>();
-    ArrayList<String> playerScore = new ArrayList<>();
-    ArrayList<Integer> id = new ArrayList<>();
-    private DBOpenHelper tdb; // Import database's assets
-    private SQLiteDatabase sdb; // Import database's assets
 
-//    final String[] from = new String[] {
-//            DatabaseHelper.ID, DatabaseHelper.NAME, DatabaseHelper.SCORE
-//    };
+    ArrayList<User> highScoreList = new ArrayList<>();
 
-//    final int[] to = new int[] {
-//            R.id.id, R.id.name, R.id.score};
-
-    public static String[] String_Array(ArrayList<String> arr)
-    {
-        String str[] = new String[arr.size()];
-        for (int j = 0; j < arr.size(); j++)
-            str[j] = arr.get(j);
-        return str;
-    } // Convert an array list to a string list for displaying the ListView
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_high_score);
-        tdb = new DBOpenHelper(this, "MIDTERM_APP.DB", null, 1);
-        sdb = tdb.getWritableDatabase();
-        recyclerView = findViewById(R.id.recyclerView);
-//        dbManager = new DBManager(this);
-//        dbManager.open();
-//        Cursor cursor = dbManager.fetch();
-//        listView = findViewById(R.id.);
-//        recyclerView = findViewById(R.id.recyclerView);
-//        adopter = new SimpleCursorAdapter(this, R.layout.score_board_view, cursor, 0)
-//        adopter.notifyDataSetChanged();
-//        recyclerView.setAdapter(adopter);
 
-        // Convert the array list to a string list
-//        String[] strlist = String_Array(noteslist);
-//
-//        // Display the list of notes (using ListView)
-//        final ListView listofnotes = findViewById(R.id.recyclerView);
-//        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
-//                android.R.layout.simple_list_item_1, strlist);
-//        listofnotes.setAdapter(adapter);
+        // data to populate the RecyclerView with
+        ArrayList<String> highScoreList = new ArrayList<>();
+
+        // set the RecyclerView
+        setUserInfo();
+        setAdopter();
+
+    }
+
+    private void setAdopter() {
+        MyAdopter adopter = new MyAdopter(highScoreList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adopter);
+    }
+    private void setUserInfo() {
+        highScoreList.add(new User("john"));
+        highScoreList.add(new User("hello"));
 
     }
 }
